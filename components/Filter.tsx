ea41020cd1593d2e570ef84a443fcca6db243e6d
Maik Emanoel <maikemanoel11@gmail.com>
2024-02-26 15:05:15 -0300

    feat: create filter component

---
+++ components/Filter.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { ChangeEvent  , useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useSearchParams, useRouter } from "next/navigation";
+
+export default function Filter() {
+  const [searchFilter, setSearchFilter] = useState('')
+
+  const router = useRouter()
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  function handleFilter() {
+    params.set('name', searchFilter)
+    router.replace(`?${params.toString()}`)
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setSearchFilter(value)
+
+    if (value === '') {
+      params.delete('name')
+      router.replace('/')
+    }
+  }
+
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="text" placeholder="Search..." className="max-w-48" onChange={handleChange}/>
+      <Button type="button" variant="outline" onClick={handleFilter}>
+        Filter
+      </Button>
+    </div>
+  );
+}
