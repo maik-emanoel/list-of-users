aab9dc32fe89ac2e0adad5062cba13157a227841
Maik Emanoel <maikemanoel11@gmail.com>
2024-03-01 10:16:30 -0300

    feat: catch searchParams and pass it to UsersTable

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
 import CreateUserDialog from "@/components/CreateUserDialog";
+import Filter from "@/components/Filter";
 import UsersTable from "@/components/UsersTable";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: {
+    name: string | undefined;
+  };
+}) {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full space-y-4">
       <header className="w-full flex justify-between">
-        <h1 className="text-2xl">Users</h1>
-
-        <CreateUserDialog />
+        <h1 className="text-3xl font-bold">Users</h1>
       </header>
 
-      <UsersTable />
+      <div className="flex justify-between">
+        <Filter />
+        <CreateUserDialog />
+      </div>
+
+      <UsersTable name={searchParams.name} />
     </div>
   );
 }
