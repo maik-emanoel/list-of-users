1c7904b67c9ace87c3031b3ca650259364085253
Maik Emanoel <maikemanoel11@gmail.com>
2024-03-04 09:53:11 -0300

    feat: create hotkey function

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,36 +1,60 @@
-'use client'
+"use client";
 
-import { ChangeEvent  , useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Filter() {
-  const router = useRouter()
+  const router = useRouter();
   const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
+  const params = useMemo(
+    () => new URLSearchParams(searchParams),
+    [searchParams]
+  );
 
-  const urlFilter = params.get("name") ?? ''
-  const [searchFilter, setSearchFilter] = useState(urlFilter)
+  const urlFilter = params.get("name") ?? "";
+  const [searchFilter, setSearchFilter] = useState(urlFilter);
 
-  function handleFilter() {
-    params.set('name', searchFilter)
-    router.replace(`?${params.toString()}`)
-  }
+  const handleFilter = useCallback(() => {
+    params.set("name", searchFilter);
+    router.replace(`?${params.toString()}`);
+  }, [params, router, searchFilter]);
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value
-    setSearchFilter(value)
+    const value = e.target.value;
+    setSearchFilter(value);
 
-    if (value === '') {
-      params.delete('name')
-      router.replace('/')
+    if (value === "") {
+      params.delete("name");
+      router.replace("/");
     }
   }
 
+  useEffect(() => {
+    function handleHotkey(e: KeyboardEvent) {
+      if (e.ctrlKey && e.key === "Enter") {
+        e.preventDefault();
+        handleFilter();
+      }
+    }
+
+    if (searchFilter !== "") {
+      window.addEventListener("keydown", handleHotkey);
+    }
+
+    return () => window.removeEventListener("keydown", handleHotkey);
+  }, [handleFilter, searchFilter]);
+
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="text" placeholder="Search..." className="max-w-48" onChange={handleChange} value={searchFilter} />
+      <Input
+        type="text"
+        placeholder="Search..."
+        className="max-w-48"
+        onChange={handleChange}
+        value={searchFilter}
+      />
       <Button type="button" variant="outline" onClick={handleFilter}>
         Filter
       </Button>
